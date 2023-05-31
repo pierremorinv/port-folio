3b6aa77295f822ab046ab6563772e72359c7dee9
Pierre Morin <morinpierrevo@gmail.com>
2023-05-31 18:27:46 +0200

    first commit

---
+++ port-folio/src/App.jsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Mon port folio </header>
+    </div>
+  );
+}
+
+export default App;
